0c3e923f9e2e25217d702f1a33657cb03522b5b3
Zachary Rump <zrrm74@mail.missouri.edu>
2016-10-06 11:03:03 -0500

    Fix ex8

--- Lab6-ex8.c
+++ Lab6-ex8.c
@@ -4,11 +4,12 @@
 int main(void)
 {
 	char *s, *dyn_s;
+	s = malloc(sizeof(s)*256);
 	int ln;
 	printf("Enter the input string\n");
 	scanf("%s",s);
 	ln = strlen(s);
-	*dyn_s = (char*)malloc(strlen(s)+1);
+	dyn_s = (char*)malloc(strlen(s)+1);
 	dyn_s = s;
 	dyn_s[strlen(s)]='\0';
 	printf(dyn_s);
