d071b5723cc4b3392663080172ccbb31bec7b616
Zachary Rump <zrrm74@mail.missouri.edu>
2016-10-06 11:01:02 -0500

    Fix ex7

--- Lab6-ex7.c
+++ Lab6-ex7.c
@@ -4,6 +4,6 @@ int main()
 	int x=1309;
 	int *p = NULL;
 	printf("\n The value of x is = %d",x);
-	printf("\n The pointer points to the value = %d",*p);
+	printf("\n The pointer points to the value = %p",p);
 	return 1;
 }
